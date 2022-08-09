10c676967c218c7586b43c760c1dff354092d592
alirezava4idi <alireza.vahidi1@gmail.com>
2022-08-09 15:22:16 +0430

    created a simple digital clock

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+const clockDiv = document.querySelector('.clock');
+let hour = document.querySelector('.hour');
+let min = document.querySelector('.min');
+let sec = document.querySelector('.sec');
+
+
+function clock() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    // document.querySelector('.clock').innerHTML = h + ":" + m + ":" + s;
+    hour.innerHTML = h;
+    min.innerHTML = m;
+    sec.innerHTML = s;
+    setTimeout(clock, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
